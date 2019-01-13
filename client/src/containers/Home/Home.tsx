0c02468649a3c959b456fb8228f5682e640c99b9
gettingold@mail.ru <bit.size.dev@gmail.com>
2019-01-13 19:17:23 +0200

    Base structure

---
+++ client/src/containers/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Button, Header } from '../../components';
+
+class Home extends Component {
+    render() {
+        return <div><Header /> HOME!!! <Button /></div>
+    }
+}
+
+export default Home;
