83e2f49401b3881b58bee35020049928e2313423
gettingold@mail.ru <bit.size.dev@gmail.com>
2019-01-13 19:27:52 +0200

    Missing containres

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 
 import Home from './Home';
+import Contacts from './Contacts';
+import Booking from './Booking';
 
 class App extends Component {
   render() {
