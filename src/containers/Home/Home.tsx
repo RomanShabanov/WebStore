e87dc7b6883d3792962a0f9f6867bf9f06cc66a2
gettingold@mail.ru <bit.size.dev@gmail.com>
2019-01-19 15:57:20 +0200

    Home Hero

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,43 +1,20 @@
 import React, { Component } from 'react';
-import Slider from "react-slick";
+import './Home.scss';
 
-import { ProductPreview } from '../../components';
+import { ProductPreview, LazyImage } from '../../components';
+
+import imageHomeHero from '../../assets/images/Test_slide.png';
 
 class Home extends Component {
     render() {
-
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
-
         return <div className="Home">
-            <div className="wrapper">
-                <h2> Single Item</h2>
-                <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
-                </Slider>
+
+            <div className="Home__Hero">
+                <div className="wrapper">
+                    <LazyImage src={imageHomeHero} />
+                </div>
             </div>
+
             <div className="wrapper">
                 <aside></aside>
                 <section>
